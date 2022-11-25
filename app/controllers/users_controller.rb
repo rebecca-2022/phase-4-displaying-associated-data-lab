2d79032c5f48adb487c5537cb4a8dcbd3fe183ea
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-11-25 12:51:56 +0300

    complted assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    #find user by id and render
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items, status: :ok
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: "User not found"}
+    end
 end
